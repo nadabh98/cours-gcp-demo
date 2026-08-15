fd9e27ac733192604f6643c871366868bf3730ac
Nada Ben Hammouda <nadabh91@gmail.com>
2026-08-15 16:45:10 +0100

    change main and requiements

---
+++ api-fastapi4/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import os
+from sqlalchemy import text
+from database import engine
+
+app = FastAPI()
+@app.get("/")
+def racine():
+ return {"message": "API FastAPI en ligne sur Cloud Run"}
+@app.get("/sante")
+def sante():
+ return {"status": "ok"}
+
+@app.get("/etudiants")
+def liste_etudiants():
+ with engine.connect() as conn:
+ resultat = conn.execute(text("SELECT NOW()"))
+ return {"heure_serveur": str(resultat.scalar())}
